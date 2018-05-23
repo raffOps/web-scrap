fc9a11903332ca022bf7f0bf5be63b7862c95e75
Rafael <rrafaeljuniorr@gmail.com>
2018-05-23 00:30:23 -0300

    todas as classes de extracao implementadas. Testes amplos em andamento

--- classes.py
+++ classes.py
@@ -2,6 +2,7 @@ import requests as req
 from bs4 import BeautifulSoup as bs
 import time
 from selenium import webdriver as wb
+import base64
 
 class SiteVendaSeminovos:
 
@@ -11,7 +12,7 @@ class SiteVendaSeminovos:
         self.__emprise_name = emprise_name
         self.__soup = None
 
-    def next_page(self):
+    def goto_next_page(self):
         self.__page_index += 1
 
     @property
@@ -108,7 +109,8 @@ class Unidas(SiteVendaSeminovos):
 class Localiza(SiteVendaSeminovos):
     def __init__(self, url, emprise_name):
         super().__init__(url, emprise_name)
-        self.__web_driver = wb.Chrome("/home/rafa/Documentos/web-scrap/chromedriver")
+        #self.__web_driver = wb.Chrome("/home/rafa/Documentos/web-scrap/chromedriver")
+        self.__web_driver = wb.PhantomJS("/home/rafa/Documentos/web-scrap/phantomjs")
         self.__web_driver.get(url)
         self.__id_next_page = "ctl00_ctl42_g_f221d036_75d3_4ee2_893d_0d7b40180245_ProximaPaginaSuperior"
         self.__finished = False
@@ -136,8 +138,48 @@ class Localiza(SiteVendaSeminovos):
     def get_price(self, car):
         return list(car[0].stripped_strings)[0][3:].replace(".", "")
 
-    def next_page(self):
+    def goto_next_page(self):
         try:
             self.__web_driver.find_element_by_id(self.__id_next_page).click()
         except:
             self.__finished = True
+
+
+class Locamerica(SiteVendaSeminovos):
+    def __init__(self, url, emprise_name):
+        super().__init__(url, emprise_name)
+
+    def set_soup(self):
+        r = req.get(self.base_url.format(self.page_index))
+        info = r.json()['veiculos']
+        site_decoded = base64.b64decode(info)
+        self.__soup = bs(site_decoded, "lxml")
+
+    def is_finished(self):
+        return not self.__soup.find(class_="clearfix")
+
+    def get_cars(self):
+        list1 = self.__soup.find_all(class_="item-carro")
+        list2 = self.__soup.findAll(class_="preco")
+
+        return [(list(car.stripped_strings), price) for car, price in zip(list1, list2)]
+
+    def get_year(self, car):
+        return car[0][3].split("/")[0]
+
+    def get_kilometragem(self, car):
+        kilometragem = car[0][7].split()[0]
+        if kilometragem.count(".") > 1:  # maior que 1000 KMs
+            kilometragem = kilometragem.replace(".", "", 1)  # tira o primeiro "."
+        if kilometragem == '-':
+            return 0
+        else:
+            return kilometragem
+
+    def get_model(self, car):
+        return car[0][1]
+
+    def get_price(self, car):
+        #print(car[1].h4.text)
+        return car[1].h4.text.replace(".", "").replace(",", ".")
+
